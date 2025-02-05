55751102350a43ea83f48b9587a6e5a1299c2113
Mike Carr <mcarr67@gmail.com>
2025-02-04 19:08:15 -0800

    add icons to header

--- OpenIPC_Config/Views/HeaderView.axaml.cs
+++ OpenIPC_Config/Views/HeaderView.axaml.cs
@@ -1,4 +1,7 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Microsoft.Extensions.DependencyInjection;
 using OpenIPC_Config.ViewModels;
 
@@ -6,10 +9,34 @@ namespace OpenIPC_Config.Views;
 
 public partial class HeaderView : UserControl
 {
+    
+    private const string TelegramLink = "https://t.me/+BMyMoolVOpkzNWUy";
+    private const string GithubLink = "https://github.com/OpenIPC/";
+    private const string DiscordLink = "https://discord.gg/KtWgDV6Y";
+    
     public HeaderView()
     {
         InitializeComponent();
 
         if (!Design.IsDesignMode) DataContext = App.ServiceProvider.GetService<MainViewModel>();
+    }
+
+
+    private void TelegramButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var launcher = TopLevel.GetTopLevel(this).Launcher;
+        launcher.LaunchUriAsync(new Uri(TelegramLink));
+    }
+
+    private void GithubButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var launcher = TopLevel.GetTopLevel(this).Launcher;
+        launcher.LaunchUriAsync(new Uri(GithubLink));
+    }
+
+    private void DiscordButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var launcher = TopLevel.GetTopLevel(this).Launcher;
+        launcher.LaunchUriAsync(new Uri(DiscordLink));
     }
 }
